1b51257ae988b4731ba702b233f5b7e283066b0a
Robiul-Haque <robiulcoc420@gmail.com>
2025-02-15 20:30:48 +0600

    install react-vite, tailwind css & daisy UI latest verson

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p className='text-red-500'>React</p>
+      <button className="btn btn-primary">Primary</button>
+    </>
+  )
+}
+
+export default App;
