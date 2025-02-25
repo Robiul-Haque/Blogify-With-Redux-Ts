580ebaf8b71b23322f61fae7d55ebd0e15fb50a0
Robiul-Haque <robiulcoc420@gmail.com>
2025-02-26 01:43:25 +0600

    creating comment section & change code in blog view modal

--- src/components/admin/ViewBlogModal.tsx
+++ src/components/admin/ViewBlogModal.tsx
@@ -1,4 +1,23 @@
+import moment from "moment";
 import { useViewBlogQuery } from "../../redux/features/admin/dashboard/dashboard";
+
+type TUserComments = {
+    _id: string;
+    blog: string;
+    comment: string;
+    createdAt: string;
+    isDeleted: boolean;
+    updateAt: string;
+    user: {
+        image: {
+            url: string;
+            publicId: string;
+        };
+        name: string;
+        role: string;
+    };
+    __V: number;
+};
 
 const ViewBlogModal = ({ id }: { id: string }) => {
     const { data, isLoading } = useViewBlogQuery(id);
@@ -11,7 +30,7 @@ const ViewBlogModal = ({ id }: { id: string }) => {
                 isLoading ?
                     <div className="skeleton w-[33%] h-96 mx-auto mt-12"></div>
                     :
-                    <div className="modal-box">
+                    <div className="modal-box h-[90%] overflow-auto">
                         <form method="dialog">
                             <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
                         </form>
@@ -23,7 +42,7 @@ const ViewBlogModal = ({ id }: { id: string }) => {
                                     <div className="flex items-center gap-x-2">
                                         <p className="capitalize text-xs roboto font-[500] bg-base-200 px-2.5 py-1 rounded-2xl">{blogData?.blog?.author?.role}</p>
                                         <p className="capitalize text-xs roboto font-[500] bg-base-200 px-2.5 py-1 rounded-2xl">Category: {blogData?.blog?.category}</p>
-                                        <div className="tooltip" data-tip={blogData?.likedUser?.map((user: any) => user.name).join(", ")}>
+                                        <div className="tooltip" data-tip={blogData?.userLike?.map((user: any) => user.name).join(", ")}>
                                             <button className="btn btn-xs inter font-[800] cursor-auto"><img className="size-4" src="https://img.icons8.com/material-outlined/24/facebook-like--v1.png" alt="facebook-like--v1" /> {blogData?.blog?.likes}</button>
                                         </div>
                                     </div>
@@ -31,8 +50,25 @@ const ViewBlogModal = ({ id }: { id: string }) => {
                             </div>
                             <div className="mt-12">
                                 <img src={blogData?.blog?.image?.url} alt={blogData?.blog?.title} className="size-60 bg-cover mx-auto" />
-                                <h3 className="inter font-[800] text-xl text-center mt-10 mb-5">{blogData?.blog?.title}</h3>
+                                <h3 className="inter font-[800] text-2xl text-center mt-10 mb-5">{blogData?.blog?.title}</h3>
                                 <p className="text-gray-700 text-base text-center">{blogData?.blog?.content}</p>
+                                <div className="max-w-2xl mx-auto p-4 mt-10 bg-white dark:bg-gray-900 rounded-lg shadow-md">
+                                    <h3 className="text-lg font-bold mb-4 text-gray-900 dark:text-white">Comments ({blogData?.userComment?.length})</h3>
+                                    <div className="space-y-6">
+                                        {blogData?.userComment?.map((comment: TUserComments) => (
+                                            <div key={comment._id} className="flex items-start space-x-4 p-3 border rounded-lg bg-gray-50 dark:bg-gray-800">
+                                                <img src={comment.user.image.url} alt={comment.user.name} className="w-12 h-12 rounded-full border-2 border-gray-300 dark:border-gray-700" />
+                                                <div className="flex-1">
+                                                    <div className="flex justify-between items-center">
+                                                        <h4 className="text-gray-900 text-sm font-semibold dark:text-white">{comment.user.name}</h4>
+                                                        <span className="text-xs text-gray-500">{moment(comment.createdAt).format("D MMM Y")}</span>
+                                                    </div>
+                                                    <p className="text-gray-700 text-sm dark:text-gray-300 mt-2 border border-gray-300 dark:border-gray-700 p-1.5 rounded-lg">{comment.comment}</p>
+                                                </div>
+                                            </div>
+                                        ))}
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
