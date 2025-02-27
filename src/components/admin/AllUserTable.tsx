80e824fa6546633724ba05b6043442b390dd3f7e
Robiul-Haque <robiulcoc420@gmail.com>
2025-02-27 13:52:35 +0600

    create user.ts api, create user status, delete function, UI & chnage some code

--- src/components/admin/AllUserTable.tsx
+++ src/components/admin/AllUserTable.tsx
@@ -1,9 +1,22 @@
+import { useAllUserQuery, useBlockUserMutation } from "../../redux/features/admin/user";
 
 const AllUserTable = () => {
+    const { data: allUsers, isLoading } = useAllUserQuery(undefined);
+    const [blockUser] = useBlockUserMutation();
+    if (isLoading) return <div>Loading...</div>
+    console.log("All User: ", allUsers.data);
+
+    const handleBlockedUser = (id: string, status: boolean) => {
+        blockUser({ id, status });
+    }
+
+    const handleDeleteUser = (id: string) => {
+        console.log("Delete User: ", id);
+    }
+
     return (
         <div className="overflow-x-auto">
-            <table className="table">
-                {/* head */}
+            <table className="table text-center">
                 <thead>
                     <tr>
                         <th>
@@ -11,144 +24,64 @@ const AllUserTable = () => {
                                 <input type="checkbox" className="checkbox" />
                             </label>
                         </th>
+                        <th>Image</th>
                         <th>Name</th>
-                        <th>Job</th>
-                        <th>Favorite Color</th>
-                        <th></th>
+                        <th>Email</th>
+                        <th>Status</th>
+                        <th>Account</th>
+                        <th>Action</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {/* row 1 */}
-                    <tr>
-                        <th>
-                            <label>
-                                <input type="checkbox" className="checkbox" />
-                            </label>
-                        </th>
-                        <td>
-                            <div className="flex items-center gap-3">
-                                <div className="avatar">
-                                    <div className="mask mask-squircle h-12 w-12">
-                                        <img
-                                            src="https://img.daisyui.com/images/profile/demo/2@94.webp"
-                                            alt="Avatar Tailwind CSS Component" />
+                    {
+                        allUsers.data.map((user: any) => (
+                            <tr key={user?._id} className="hover:bg-gray-50">
+                                <th>
+                                    <label>
+                                        <input type="checkbox" className="checkbox" />
+                                    </label>
+                                </th>
+                                <td>
+                                    <img src={user?.image?.url} alt={user?.name} className="size-14 rounded-full border mx-auto" />
+                                </td>
+                                <td>
+                                    <div className="flex items-center flex-col gap-y-2 inter font-[600]">
+                                        <span>{user?.name}</span>
+                                        <p className="text-green-400 bg-green-100/50 text-xs badge capitalize">{user?.role}</p>
                                     </div>
-                                </div>
-                                <div>
-                                    <div className="font-bold">Hart Hagerty</div>
-                                    <div className="text-sm opacity-50">United States</div>
-                                </div>
-                            </div>
-                        </td>
-                        <td>
-                            Zemlak, Daniel and Leannon
-                            <br />
-                            <span className="badge badge-ghost badge-sm">Desktop Support Technician</span>
-                        </td>
-                        <td>Purple</td>
-                        <th>
-                            <button className="btn btn-ghost btn-xs">details</button>
-                        </th>
-                    </tr>
-                    {/* row 2 */}
-                    <tr>
-                        <th>
-                            <label>
-                                <input type="checkbox" className="checkbox" />
-                            </label>
-                        </th>
-                        <td>
-                            <div className="flex items-center gap-3">
-                                <div className="avatar">
-                                    <div className="mask mask-squircle h-12 w-12">
-                                        <img
-                                            src="https://img.daisyui.com/images/profile/demo/3@94.webp"
-                                            alt="Avatar Tailwind CSS Component" />
+                                </td>
+                                <td>
+                                    <p>{user?.email}</p>
+                                </td>
+                                <td className="w-30">
+                                    <div className="flex justify-center items-center flex-col gap-y-2">
+                                        <label className={`${user?.isBlocked ? "border-red-400" : "border-green-400"} toggle text-base-content border rounded-full`}>
+                                            <input type="checkbox" onChange={(e) => handleBlockedUser(user?._id, e.target.checked)} />
+                                            <svg className="bg-green-400 rounded-full" aria-label="enabled" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="4" fill="none" stroke="currentColor"><path d="M20 6 9 17l-5-5"></path></g></svg>
+                                            <svg className="bg-red-400 rounded-full" aria-label="disabled" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"><path d="M18 6 6 18" /><path d="m6 6 12 12" /></svg>
+                                        </label>
+                                        <p>{user?.isBlocked ? <p className="text-red-400 bg-red-100/40 text-xs badge font-bold">Block</p>
+                                            :
+                                            <p className="text-green-400 bg-green-100/40 text-xs badge font-bold">Active</p>}</p>
                                     </div>
-                                </div>
-                                <div>
-                                    <div className="font-bold">Brice Swyre</div>
-                                    <div className="text-sm opacity-50">China</div>
-                                </div>
-                            </div>
-                        </td>
-                        <td>
-                            Carroll Group
-                            <br />
-                            <span className="badge badge-ghost badge-sm">Tax Accountant</span>
-                        </td>
-                        <td>Red</td>
-                        <th>
-                            <button className="btn btn-ghost btn-xs">details</button>
-                        </th>
-                    </tr>
-                    {/* row 3 */}
-                    <tr>
-                        <th>
-                            <label>
-                                <input type="checkbox" className="checkbox" />
-                            </label>
-                        </th>
-                        <td>
-                            <div className="flex items-center gap-3">
-                                <div className="avatar">
-                                    <div className="mask mask-squircle h-12 w-12">
-                                        <img
-                                            src="https://img.daisyui.com/images/profile/demo/4@94.webp"
-                                            alt="Avatar Tailwind CSS Component" />
-                                    </div>
-                                </div>
-                                <div>
-                                    <div className="font-bold">Marjy Ferencz</div>
-                                    <div className="text-sm opacity-50">Russia</div>
-                                </div>
-                            </div>
-                        </td>
-                        <td>
-                            Rowe-Schoen
-                            <br />
-                            <span className="badge badge-ghost badge-sm">Office Assistant I</span>
-                        </td>
-                        <td>Crimson</td>
-                        <th>
-                            <button className="btn btn-ghost btn-xs">details</button>
-                        </th>
-                    </tr>
-                    {/* row 4 */}
-                    <tr>
-                        <th>
-                            <label>
-                                <input type="checkbox" className="checkbox" />
-                            </label>
-                        </th>
-                        <td>
-                            <div className="flex items-center gap-3">
-                                <div className="avatar">
-                                    <div className="mask mask-squircle h-12 w-12">
-                                        <img
-                                            src="https://img.daisyui.com/images/profile/demo/5@94.webp"
-                                            alt="Avatar Tailwind CSS Component" />
-                                    </div>
-                                </div>
-                                <div>
-                                    <div className="font-bold">Yancy Tear</div>
-                                    <div className="text-sm opacity-50">Brazil</div>
-                                </div>
-                            </div>
-                        </td>
-                        <td>
-                            Wyman-Ledner
-                            <br />
-                            <span className="badge badge-ghost badge-sm">Community Outreach Specialist</span>
-                        </td>
-                        <td>Indigo</td>
-                        <th>
-                            <button className="btn btn-ghost btn-xs">details</button>
-                        </th>
-                    </tr>
+                                </td>
+                                <th>
+                                    {
+                                        user?.isVerified ?
+                                            <p className="text-green-400 font-bold">Verified</p>
+                                            :
+                                            <p className="text-red-400 bg-red-100/40 text-xs badge font-bold">Not Verified</p>
+                                    }
+                                </th>
+                                <th>
+                                    <button onClick={() => handleDeleteUser(user?._id)} title="Delete" className="btn btn-sm bg-red-200 hover:bg-red-400"><img className="size-5" src="https://img.icons8.com/material-rounded/50/filled-trash.png" alt="filled-trash" /></button>
+                                </th>
+                            </tr>
+                        ))
+                    }
                 </tbody>
             </table>
-        </div>
+        </div >
     )
 }
 
---
+++ src/redux/features/admin/user.ts
@@ -0,0 +1,30 @@
+import { baseApi } from "../../api/baseApi";
+
+const userApi = baseApi.injectEndpoints({
+    endpoints: (build) => ({
+        AllUser: build.query({
+            query: () => ({
+                url: "/user/admin-get-all-user",
+                method: "GET",
+            }),
+            providesTags: ["user"],
+        }),
+        BlockUser: build.mutation({
+            query: (data: { id: string, status: boolean }) => ({
+                url: "/user/admin-user-blocked",
+                method: "PATCH",
+                body: data,
+            }),
+            invalidatesTags: ["user"],
+        }),
+        DeleteUser: build.mutation({
+            query: (id: string) => ({
+                url: `/user/admin-delete-user/${id}`,
+                method: "DELETE",
+            }),
+            invalidatesTags: ["user"],
+        }),
+    })
+});
+
+export const { useAllUserQuery, useBlockUserMutation, useDeleteUserMutation } = userApi;
