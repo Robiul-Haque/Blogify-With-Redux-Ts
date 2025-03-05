55aedd48a32241384092bd1a448ccf654ba41716
Robiul-Haque <robiulcoc420@gmail.com>
2025-03-06 02:08:57 +0600

    complete editBlogModal & change AllBlogEditCode

--- src/components/admin/AllBlogTable.tsx
+++ src/components/admin/AllBlogTable.tsx
@@ -23,77 +23,81 @@ type TBlog = {
     createdAt: string;
     updatedAt: string;
 }
-
 const AllBlogTable = () => {
     const { data, isLoading } = useAllBlogQuery(undefined);
     const { data: AllBlog } = data || {};
     const [blogId, setBlogId] = useState<string>("");
 
-    if (isLoading) return <h2>Loading...</h2>;
-
     return (
-        <section className="mt-1">
-            <div className="overflow-x-auto">
-                <table className="table text-center">
-                    <thead>
-                        <tr>
-                            <th>
-                                <label>
-                                    <input type="checkbox" className="checkbox" />
-                                </label>
-                            </th>
-                            <th>Image</th>
-                            <th>Name</th>
-                            <th>Category</th>
-                            <th><img className="size-3.5" title="Like" src="https://img.icons8.com/fluency-systems-filled/50/facebook-like.png" alt="facebook-like" /></th>
-                            <th><img className="size-3.5" title="Comment" src="https://img.icons8.com/ios-glyphs/30/speech-bubble--v1.png" alt="speech-bubble--v1" /></th>
-                            <th>Status</th>
-                            <th>Author</th>
-                            <th>Create</th>
-                            <th>Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            AllBlog?.map((blog: TBlog) => (
-                                <tr key={blog?._id} className="hover:bg-gray-50">
-                                    <td>
-                                        <label>
-                                            <input type="checkbox" className="checkbox" />
-                                        </label>
-                                    </td>
-                                    <td>
-                                        <img src={blog?.image?.url} alt={blog.title} className="size-11" />
-                                    </td>
-                                    <td title={blog?.title} className="text-xs">{blog?.title?.split(" ").length > 10 ? `${blog?.title?.split(" ").slice(0, 10).join(" ")}...` : blog.title}</td>
-                                    <td title={blog?.category} className="text-xs">{blog?.category}</td>
-                                    <td className="font-semibold text-xs">{blog?.likes}</td>
-                                    <td className="font-semibold text-xs">{blog?.comments?.length}</td>
-                                    <td>{blog?.isPublished ? <p className="text-green-500 bg-green-100/50 font-bold text-xs badge badge-sm">Publish</p> : <p className="text-red-500 bg-red-100/50 font-bold text-xs badge badge-sm">Not Publish</p>}</td>
-                                    <td className="font-semibold text-gray-500 text-xs">{blog?.author?.name}</td>
-                                    <td className="text-gray-600 text-xs font-semibold">{moment(blog?.createdAt).format("D MMM Y")}</td>
-                                    <td className="flex justify-center max-sm:flex-col gap-3 mt-2">
-                                        <button onClick={() => {
-                                            setBlogId(blog?._id);
-                                            const modal = document.getElementById("view_blog_modal");
-                                            if (modal) (modal as HTMLDialogElement).showModal();
-                                        }} title="View" className="btn btn-success btn-xs"><img className="size-5" src="https://img.icons8.com/ios-glyphs/30/FFFFFF/visible--v1.png" alt="visible--v1" /></button>
-                                        <button onClick={() => {
-                                            setBlogId(blog?._id);
-                                            const modal = document.getElementById("edit_blog_modal");
-                                            if (modal) (modal as HTMLDialogElement).showModal();
-                                        }} title="Edit" className="btn btn-primary btn-xs"><img className="size-5" src="https://img.icons8.com/material-sharp/24/FFFFFF/edit--v1.png" alt="edit--v1" /></button>
-                                        <button title="Delete" className="btn btn-error btn-xs"><img className="size-5" src="https://img.icons8.com/material-rounded/24/FFFFFF/filled-trash.png" alt="filled-trash" /></button>
-                                    </td>
-                                </tr>
-                            ))
-                        }
-                    </tbody>
-                </table>
-            </div>
-            <ViewBlogModal id={blogId} />
-            <EditBlogModal id={blogId} />
-        </section>
+        <>
+            {
+                isLoading ?
+                    <div className="skeleton w-[99%] h-80 mx-auto mt-1"></div>
+                    :
+                    <section className="mt-1">
+                        <div className="overflow-x-auto">
+                            <table className="table text-center">
+                                <thead>
+                                    <tr>
+                                        <th>
+                                            <label>
+                                                <input type="checkbox" className="checkbox" />
+                                            </label>
+                                        </th>
+                                        <th>Image</th>
+                                        <th>Name</th>
+                                        <th>Category</th>
+                                        <th><img className="size-3.5" title="Like" src="https://img.icons8.com/fluency-systems-filled/50/facebook-like.png" alt="facebook-like" /></th>
+                                        <th><img className="size-3.5" title="Comment" src="https://img.icons8.com/ios-glyphs/30/speech-bubble--v1.png" alt="speech-bubble--v1" /></th>
+                                        <th>Status</th>
+                                        <th>Author</th>
+                                        <th>Create</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        AllBlog?.map((blog: TBlog) => (
+                                            <tr key={blog?._id} className="hover:bg-gray-50">
+                                                <td>
+                                                    <label>
+                                                        <input type="checkbox" className="checkbox" />
+                                                    </label>
+                                                </td>
+                                                <td>
+                                                    <img src={blog?.image?.url} alt={blog.title} className="size-11" />
+                                                </td>
+                                                <td title={blog?.title} className="text-xs">{blog?.title?.split(" ").length > 10 ? `${blog?.title?.split(" ").slice(0, 10).join(" ")}...` : blog.title}</td>
+                                                <td title={blog?.category} className="text-xs">{blog?.category}</td>
+                                                <td className="font-semibold text-xs">{blog?.likes}</td>
+                                                <td className="font-semibold text-xs">{blog?.comments?.length}</td>
+                                                <td>{blog?.isPublished ? <p className="text-green-500 bg-green-100/50 font-bold text-xs badge badge-sm">Publish</p> : <p className="text-red-500 bg-red-100/50 font-bold text-xs badge badge-sm">Not Publish</p>}</td>
+                                                <td className="font-semibold text-gray-500 text-xs">{blog?.author?.name}</td>
+                                                <td className="text-gray-600 text-xs font-semibold">{moment(blog?.createdAt).format("D MMM Y")}</td>
+                                                <td className="flex justify-center max-sm:flex-col gap-3 mt-2">
+                                                    <button onClick={() => {
+                                                        setBlogId(blog?._id);
+                                                        const modal = document.getElementById("view_blog_modal");
+                                                        if (modal) (modal as HTMLDialogElement).showModal();
+                                                    }} title="View" className="btn btn-success btn-xs"><img className="size-5" src="https://img.icons8.com/ios-glyphs/30/FFFFFF/visible--v1.png" alt="visible--v1" /></button>
+                                                    <button onClick={() => {
+                                                        setBlogId(blog?._id);
+                                                        const modal = document.getElementById("edit_blog_modal");
+                                                        if (modal) (modal as HTMLDialogElement).showModal();
+                                                    }} title="Edit" className="btn btn-primary btn-xs"><img className="size-5" src="https://img.icons8.com/material-sharp/24/FFFFFF/edit--v1.png" alt="edit--v1" /></button>
+                                                    <button title="Delete" className="btn btn-error btn-xs"><img className="size-5" src="https://img.icons8.com/material-rounded/24/FFFFFF/filled-trash.png" alt="filled-trash" /></button>
+                                                </td>
+                                            </tr>
+                                        ))
+                                    }
+                                </tbody>
+                            </table>
+                        </div>
+                        <ViewBlogModal id={blogId} />
+                        <EditBlogModal id={blogId} />
+                    </section>
+            }
+        </>
     )
 }
 
--- src/components/admin/EditBlogModal.tsx
+++ src/components/admin/EditBlogModal.tsx
@@ -1,9 +1,37 @@
+import { useForm } from "react-hook-form";
 import { useViewBlogForUpdateQuery } from "../../redux/features/admin/blog";
+import { useEffect } from "react";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const blogUpdateSchema = z.object({
+    title: z.string(),
+    category: z.string(),
+    content: z.string(),
+});
+
+type Inputs = z.infer<typeof blogUpdateSchema>;
 
 const EditBlogModal = ({ id }: { id: string }) => {
     const { data, isLoading } = useViewBlogForUpdateQuery(id);
     const { data: blogData } = data || {};
-    console.log(blogData);
+    const { register, handleSubmit, reset, } = useForm<Inputs>(
+        {
+            resolver: zodResolver(blogUpdateSchema),
+        }
+    );
+
+    useEffect(() => {
+        reset({
+            title: blogData?.title || "",
+            category: blogData?.category || "",
+            content: blogData?.content || ""
+        });
+    }, [blogData, reset]);
+
+    const onSubmit = (data: any) => {
+        console.log(data);
+    }
 
     return (
         <dialog id="edit_blog_modal" className="modal">
@@ -15,16 +43,24 @@ const EditBlogModal = ({ id }: { id: string }) => {
                         <form method="dialog">
                             <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
                         </form>
-                        <h1>Edit Blog modal id: {id}</h1>
-                        <div className="card-body mx-auto w-[90%]">
-                            <fieldset className="fieldset mx-auto w-[90%]">
-                                <label className="fieldset-label">Email</label>
-                                <input type="email" className="input" placeholder="Email" />
-                                <label className="fieldset-label">Password</label>
-                                <input type="password" className="input" placeholder="Password" />
-                                <button className="btn btn-neutral mt-4">Login</button>
+                        <form onSubmit={handleSubmit(onSubmit)} className="card-body mx-auto w-full">
+                            <fieldset className="fieldset mx-auto w-full">
+                                <div>
+                                    <label className="fieldset-label mb-0.5">Image</label>
+                                    <span className="flex justify-between items-center gap-2">
+                                        <input type="file" className="file-input" />
+                                        <img className="size-11" src={blogData?.image?.url} alt={blogData?.category} />
+                                    </span>
+                                </div>
+                                <label className="fieldset-label mt-3">Title</label>
+                                <input type="text" className="input w-full" {...register("title", { required: true, min: 1 })} />
+                                <label className="fieldset-label mt-3">Category</label>
+                                <input type="text" className="input w-full" {...register("category", { required: true, min: 1 })} />
+                                <label className="fieldset-label mt-3">Content</label>
+                                <textarea className="textarea w-full" {...register("content", { required: true, min: 1 })} />
+                                <button type="submit" className="btn btn-neutral mt-4">Update</button>
                             </fieldset>
-                        </div>
+                        </form>
                     </div>
             }
         </dialog>
