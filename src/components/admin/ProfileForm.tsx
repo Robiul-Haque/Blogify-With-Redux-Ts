3eba24bc61ae7d9411cce74404f7088a8ba0fcba
Robiul-Haque <robiulcoc420@gmail.com>
2025-03-10 22:13:08 +0600

    create profileForm.tsx component & use it profile page

---
+++ src/components/admin/ProfileForm.tsx
@@ -0,0 +1,26 @@
+
+const ProfileForm = () => {
+    return (
+        <form className="flex items-center flex-col">
+            <div className="w-[90%] px-5.5">
+                <label className="fieldset-label mb-2.5">Name</label>
+                <input type="text" className="input" />
+            </div>
+
+            <div className="w-[90%] px-5.5 py-5 relative">
+                <label className="fieldset-label mb-2.5">Email</label>
+                <input type="email" className="input focus:outline-none focus:border-gray-300" readOnly title="Can,t change this field value" />
+                <p className="badge badge-soft badge-neutral badge-xs font-semibold absolute right-10 top-10">Admin</p>
+            </div>
+
+            <div className="w-[90%] px-5.5 pb-4">
+                <label className="fieldset-label mb-2.5">Image</label>
+                <input type="file" className="file-input" />
+            </div>
+
+            <button className="btn btn-neutral mt-4">Update</button>
+        </form>
+    )
+}
+
+export default ProfileForm;
--- src/pages/admin/Profile.tsx
+++ src/pages/admin/Profile.tsx
@@ -1,9 +1,13 @@
+import ProfileForm from "../../components/admin/ProfileForm";
 
 const Profile = () => {
     return (
-        <div>
-            Profile Page
-        </div>
+        <section className="flex justify-center items-center h-full">
+            <fieldset className="fieldset w-sm bg-base-200 border border-base-300 px-2 py-8 rounded-box">
+                <legend className="fieldset-legend text-xl">Profile Management</legend>
+                <ProfileForm />
+            </fieldset>
+        </section >
     )
 }
 
